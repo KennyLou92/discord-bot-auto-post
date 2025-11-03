e02e7cd506b4d93084bb02570a3ff50fe7fb3ad2
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2025-11-03 09:18:05 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -37,6 +37,8 @@ def generate_urls(version):
         "box/list_03_l_tw.png",
         "box/list_04_l_tw.png",
         "box/list_05_l_tw.png",
+        "settsum/settsum_tw_01.png",
+        "settsum/settsum_tw_02.png",
         "pair/pair_tw_01.png",
         "pair/pair_tw_02.png",
         "select/select01_head_tw.png",       
